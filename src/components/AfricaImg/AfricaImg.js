32df992f17bb15bd045aed33f04f7d18915b39af
Monalisa017 <61711279+Monalisa017@users.noreply.github.com>
2024-05-19 00:22:04 -0300

    fix: Ajuste de imagem Estruturação da Home

--- src/components/AfricaImg/AfricaImg.js
+++ src/components/AfricaImg/AfricaImg.js
@@ -4,14 +4,14 @@ import styled from 'styled-components';
 import africa from '../../assets/imagens/africa.png';
 
 const ImagemEstilizada = styled.img`
-  width: 90px;
-  height : 90px;
-  margin: auto;
-  margin-top: -15px;
+  width: 85px;
+  height : 85px;
+  margin-left: 80%;
+  margin-top: -10%;
   border-radius: 50%;
   box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
 `;
 
 export default function Imagem() {
-  return <ImagemEstilizada src={africa} alt="Descrição da imagem" />;
+  return <ImagemEstilizada src={africa} alt="Africa" />;
 }
--- src/pages/Home/home.js
+++ src/pages/Home/home.js
@@ -1,14 +1,53 @@
-import { createTheme } from '@mui/material/styles';
+import * as React from 'react';
+import PropTypes from 'prop-types';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import CssBaseline from '@mui/material/CssBaseline';
+import Divider from '@mui/material/Divider';
+import Drawer from '@mui/material/Drawer';
+import IconButton from '@mui/material/IconButton';
+import { List, ListItem, ListItemText, ListItemButton } from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import styled from 'styled-components';
+import Imagem from '../../assets/imagens/africa.png';
+import { Grid } from '@mui/material';
+import { useState } from 'react';
+import background from '../../assets/imagens/background.png';
 
-const theme = createTheme({
-  palette: {
-    primary: {
-      main: '#1976d2',
-    },
-    secondary: {
-      main: '#dc004e',
-    },
-  },
-});
 
-export default theme;
+function Home(props) {
+  const { window } = props;
+  const [mobileOpen, setMobileOpen] = React.useState(true);
+
+
+
+  return (
+    <Box
+      component="div"
+      sx={{
+        backgroundImage: `url(${background})`,
+        backgroundColor: 'transparent',
+        backgroundSize: 'cover',
+        minWidth: '100vh',
+        minHeight: '100vh',
+        marginTop:'50px'
+      }}
+    >
+      <Grid container>
+        <Grid item xs={6} sm={6} md={6} lg={6}>
+
+          <Typography> TESTE </Typography>
+        </Grid>
+        <Grid itemxs={6} sm={6} md={6} lg={6} >
+
+          <Typography> TESTE2 </Typography>
+        </Grid>
+      </Grid>
+    </Box>
+  );
+}
+
+export default Home;
